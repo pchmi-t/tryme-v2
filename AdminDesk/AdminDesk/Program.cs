af3ab75defb4862ff866d8cb02fbfec73b7f6ff3
EmineBasheva <eminka23@abv.bg>
2017-01-05 18:57:23 +0200

    add AdminDesk project

---
+++ AdminDesk/AdminDesk/Program.cs
@@ -0,0 +1,36 @@
+﻿using Admindesk;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminDesk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            //var client = new Admindesk.AdminDesk.AdminDeskClient(channel);
+
+            //var subjectsRequest = client.GetSubjects(new IdRequest());
+            //Console.WriteLine("Number of subjects: " + subjectsRequest.Subjects.Count);
+
+            //var classesRequest = client.GetClasses(new IdRequest());
+            //Console.WriteLine("Number of classes: " + classesRequest.Classes.Count);
+
+            //var testNamesRequest = client.GetTestNames(new IdRequest());
+            //Console.WriteLine("Number of testNames: " + testNamesRequest.Themes.Count);
+
+            //var testsRequest = client.GetTest(new IdRequest() { Id = "001" });
+            //Console.WriteLine("Number of questions in the test: " + testsRequest.Questions.Count);
+
+            //channel.ShutdownAsync().Wait();
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ AdminDesk/AdminDesk_Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using System;
+
+namespace AdminDesk_Server
+{
+    class Program
+    {
+        const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Admindesk.AdminDesk.BindService(new AdminDeskServer()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("AdminDesk server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
